4761c7082d3126363bad4a12ddff766915e61d50
João Paulo Cercal <jpcercal@gmail.com>
2015-12-09 18:07:37 -0200

    Added the GteExpr class to perform queries (greater than).

---
+++ src/Expr/GtExpr.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the Cekurte package.
+ *
+ * (c) João Paulo Cercal <jpcercal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cekurte\Resource\Query\Language\Expr;
+
+use Cekurte\Resource\Query\Language\Contract\ExprInterface;
+use Cekurte\Resource\Query\Language\Expr\GteExpr;
+
+/**
+ * GtExpr
+ *
+ * @author João Paulo Cercal <jpcercal@gmail.com>
+ */
+class GtExpr extends GteExpr implements ExprInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function __construct($field, $value)
+    {
+        parent::__construct($field, $value);
+
+        $this->expression = 'gt';
+        $this->operator   = '>';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getName()
+    {
+        return 'Greater than';
+    }
+}
