e873a2a9f5738a55e3555ae188f7a5969cdf079b
João Paulo Cercal <jpcercal@gmail.com>
2015-12-09 18:01:09 -0200

    Added the Exception subtypes

---
+++ src/Exception/ExprException.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Cekurte package.
+ *
+ * (c) João Paulo Cercal <jpcercal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cekurte\Resource\Query\Language\Exception;
+
+use Cekurte\Resource\Query\Language\Exception\QueryLanguageException;
+
+/**
+ * ExprException
+ *
+ * @author João Paulo Cercal <jpcercal@gmail.com>
+ */
+class ExprException extends QueryLanguageException
+{
+
+}
---
+++ src/Exception/ParserException.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Cekurte package.
+ *
+ * (c) João Paulo Cercal <jpcercal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cekurte\Resource\Query\Language\Exception;
+
+use Cekurte\Resource\Query\Language\Exception\QueryLanguageException;
+
+/**
+ * ParserException
+ *
+ * @author João Paulo Cercal <jpcercal@gmail.com>
+ */
+class ParserException extends QueryLanguageException
+{
+
+}
