4ab05c009730f4543d6e8acbb90d78ec1ad56e9a
João Paulo Cercal <jpcercal@gmail.com>
2015-12-09 17:59:49 -0200

    Added a interface that defines a Processor

---
+++ src/Contract/ProcessorInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Cekurte package.
+ *
+ * (c) João Paulo Cercal <jpcercal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cekurte\Resource\Query\Language\Contract;
+
+use Cekurte\Resource\Query\Language\ExprQueue;
+
+/**
+ * ProcessorInterface
+ *
+ * @author João Paulo Cercal <jpcercal@gmail.com>
+ */
+interface ProcessorInterface
+{
+    /**
+     * Process a ExprQueue to a specific target.
+     *
+     * @param  ExprQueue $queue
+     *
+     * @return mixed
+     */
+    public function process(ExprQueue $queue);
+}
