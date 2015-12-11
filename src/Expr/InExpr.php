472a9016c1c17f3899e812b59945c745df780aa3
João Paulo Cercal <jpcercal@gmail.com>
2015-12-11 17:17:58 -0200

    Added the InExpr unit tests.

--- src/Expr/InExpr.php
+++ src/Expr/InExpr.php
@@ -12,6 +12,7 @@
 namespace Cekurte\Resource\Query\Language\Expr;
 
 use Cekurte\Resource\Query\Language\Contract\ExprInterface;
+use Cekurte\Resource\Query\Language\Exception\InvalidExprException;
 use Cekurte\Resource\Query\Language\Expr\AbstractExpr;
 
 /**
@@ -24,29 +25,67 @@ class InExpr extends AbstractExpr implements ExprInterface
     /**
      * @var array
      */
-    protected $values;
+    protected $rawValue;
 
     /**
      * @param  string       $field
-     * @param  string|array $values
+     * @param  string|array $rawValue
      */
-    public function __construct($field, $values)
+    public function __construct($field, $rawValue)
     {
+        if (!is_string($field)) {
+            throw new InvalidExprException('The value of "field" must be a string data type.');
+        }
+
+        if (empty($field)) {
+            throw new InvalidExprException('The value of "field" can not be empty.');
+        }
+
+        if ((is_string($rawValue) || is_array($rawValue)) && empty($rawValue)) {
+            throw new InvalidExprException('The value of "rawValue" can not be empty.');
+        }
+
+        $validate = function ($rawValue) {
+            if (is_bool($rawValue)) {
+                throw new InvalidExprException('The value of "rawValue" contains a bool data type.');
+            }
+
+            if (is_null($rawValue)) {
+                throw new InvalidExprException('The value of "rawValue" contains a null data type.');
+            }
+        };
+
+        if (is_array($rawValue)) {
+            foreach ($rawValue as $item) {
+                if (is_array($item)) {
+                    throw new InvalidExprException('The value of "rawValue" contains an array data type.');
+                }
+
+                if (is_string($item) && empty($item)) {
+                    throw new InvalidExprException('The value of "rawValue" contains an empty value.');
+                }
+
+                $validate($item);
+            }
+        } else {
+            $validate($rawValue);
+        }
+
         $this->expression = 'in';
         $this->operator   = 'in';
 
         $this->field = $field;
-        $this->value = is_array($values) ? implode('+', $values) : $values;
+        $this->value = is_array($rawValue) ? implode('+', $rawValue) : $rawValue;
 
-        $this->values = $values;
+        $this->rawValue = $rawValue;
     }
 
     /**
      * @return array
      */
-    public function getValues()
+    public function getRawValue()
     {
-        return $this->values;
+        return $this->rawValue;
     }
 
     /**
